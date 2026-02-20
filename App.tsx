e63a03cb69fb2c68488f7b830b8e6f2ca0cf630e
jubayer946 <m.jubayer111@gmail.com>
2026-02-20 23:10:29 +0600

    Add Dashboard, Sales, and Expenses views to App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import InventoryView from './views/InventoryView';
+import { DashboardView } from './views/DashboardView';
+import InventoryView from './views/InventoryView';      // ← No curly braces
+import { SalesView } from './views/SalesView';
+import { ExpensesView } from './views/ExpensesView';
 
 const App = () => {
     return (
         <div>
             <h1>Business Money Tracker</h1>
+            <DashboardView />
             <InventoryView />
+            <SalesView />
+            <ExpensesView />
         </div>
     );
 };
