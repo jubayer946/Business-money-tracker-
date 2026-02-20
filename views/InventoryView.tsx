f7352c40182d8d9e5368df52edcfe82698baf3b4
jubayer946 <m.jubayer111@gmail.com>
2026-02-20 22:56:33 +0600

    Update InventoryView component to use default export

--- views/InventoryView.tsx
+++ views/InventoryView.tsx
@@ -3,22 +3,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getInventory } from '../actions/inventoryActions';
 import InventoryItem from './InventoryItem';
 
-export const InventoryView = () => {
-  const dispatch = useDispatch();
-  const inventory = useSelector(state => state.inventory);
+const InventoryView = () => {
+    const dispatch = useDispatch();
+    const inventory = useSelector(state => state.inventory);
 
-  React.useEffect(() => {
-    dispatch(getInventory());
-  }, [dispatch]);
+    React.useEffect(() => {
+        dispatch(getInventory());
+    }, [dispatch]);
 
-  return (
-    <div className="bg-white dark:bg-gray-800 p-4 rounded shadow">
-      <h1 className="text-2xl font-bold dark:text-slate-50">Inventory</h1>
-      <ul className="list-disc list-inside">
-        {inventory.map(item => (
-          <InventoryItem key={item.id} item={item} />
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="bg-white dark:bg-gray-800 p-4 rounded shadow">
+            <h1 className="text-2xl font-bold dark:text-slate-50">Inventory</h1>
+            <ul className="list-disc list-inside">
+                {inventory.map(item => (
+                    <InventoryItem key={item.id} item={item} />
+                ))}
+            </ul>
+        </div>
+    );
 };
+
+export default InventoryView;
