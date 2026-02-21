6a4b8c5881a71ea7a0c6eb214b6c5382048c19de
jubayer946 <m.jubayer111@gmail.com>
2026-02-21 13:33:34 +0600

    Update InventoryView.tsx

--- views/InventoryView.tsx
+++ views/InventoryView.tsx
@@ -5,7 +5,7 @@ import InventoryItem from './InventoryItem';
 
 const InventoryView = () => {
     const dispatch = useDispatch();
-    const inventory = useSelector(state => state.inventory);
+    const inventory = useSelector((state: any) => state.inventory);
 
     React.useEffect(() => {
         dispatch(getInventory());
@@ -15,7 +15,7 @@ const InventoryView = () => {
         <div className="bg-white dark:bg-gray-800 p-4 rounded shadow">
             <h1 className="text-2xl font-bold dark:text-slate-50">Inventory</h1>
             <ul className="list-disc list-inside">
-                {inventory.map(item => (
+                {inventory.map((item: any) => (
                     <InventoryItem key={item.id} item={item} />
                 ))}
             </ul>
